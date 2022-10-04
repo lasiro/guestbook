4bee35ea52fa9c1ded71bfa89c7b9bd8e95b5e71
lasiro <messiekimbembe01@gmail.com>
2022-10-04 17:30:49 +0200

    update conference controller

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -4,17 +4,23 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $greet='';
+        if($name=$request->query->get('hello')){
+            $greet=sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name)) ;}
         return new Response(<<<EOF
         <html>
            <body>
+
             <h1>Site en construction</h1>
+          <em>  {$greet} </em>
            <img src="/images/underConstruction.gif" />
            </body>
         </html>
